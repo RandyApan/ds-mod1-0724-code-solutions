5425e34c60c005e090feaba0e6c55b41bf70e92f
Randy Apan <randyaapan@gmail.com>
2024-07-31 15:15:11 -0700

    Python-dictionaries ready

--- python-dictionaries/python_dictionaries_solution.py
+++ python-dictionaries/python_dictionaries_solution.py
@@ -1,8 +1,21 @@
-def binary_dict(number):
-  d = {}
+def decimal_to_bin(dec):
+  binary = ''
+  while dec > 0:
+    binary = str(dec % 2)+ binary
+    dec = dec // 2
+  return binary
+
+print(decimal_to_bin(15))
+
+def binary_dict(input):
+  binaryD = {}
   _ = 0
-  while _ <= number:
-    d[_] = bin(number)
-    _ = _ + 1
-  print(d)
-binary_dict(15)
+  if  _ == 0:
+    binaryD[_] = '0'
+    _ += 1
+  while _ <= input:
+    binaryD[_] = decimal_to_bin(_)
+    _ += 1
+  return binaryD
+
+print(binary_dict(25))
